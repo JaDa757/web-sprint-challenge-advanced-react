edd5d887b1d4aa52f4ec7e364d22eda2a6beab2f
Joshua Ackerman <joshackerman757@gmail.com>
2023-09-20 17:25:23 -0400

    day one changes. stil need onSubmit method

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,21 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
-
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
-
 export default class AppClass extends React.Component {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      email: '',
+      index: 4,
+      steps: 0,
+    };
+  }
+
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
@@ -29,27 +25,90 @@ export default class AppClass extends React.Component {
   }
 
   reset = () => {
-    // Use this helper to reset all states to their initial values.
-  }
+    this.setState({
+      message: '',
+      email: '',
+      index: 4,
+      steps: 0,
+    });
+  };
 
   getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
+    // Define the dimensions of your grid
+    const numRows = 3;
+    const numCols = 3;
 
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+    // Calculate the current row and column of "B" based on the current index
+    const currentRow = Math.floor(this.state.index / numCols);
+    const currentCol = this.state.index % numCols;
+
+    // Define a mapping of directions to their corresponding row and column changes
+    const directionMap = {
+      left: { rowChange: 0, colChange: -1 },
+      up: { rowChange: -1, colChange: 0 },
+      right: { rowChange: 0, colChange: 1 },
+      down: { rowChange: 1, colChange: 0 },
+    };
+
+    // Get the row and column changes for the given direction
+    const { rowChange, colChange } = directionMap[direction];
+
+    // Calculate the new row and column based on the direction
+    const nextRow = currentRow + rowChange;
+    const nextCol = currentCol + colChange;
+
+    // Calculate the new index
+    const nextIndex = nextRow * numCols + nextCol;
+
+    // Ensure the new index is within the grid boundaries
+    return Math.max(0, Math.min(numRows * numCols - 1, nextIndex));
+  };
+
+  move = (direction) => {
+    const nextIndex = this.getNextIndex(direction);
+    if (nextIndex !== this.state.index) {
+      this.setState((prevState) => ({
+        index: nextIndex,
+        steps: prevState.steps + 1,
+        message: `Moved ${direction}`,
+      }))
+    }
   }
 
   onChange = (evt) => {
-    // You will need this to update the value of the input.
+    this.setState({ email: evt.target.value });
   }
 
-  onSubmit = (evt) => {
-    // Use a POST request to send a payload to the server.
+  onSubmit = async (evt) => {
+    evt.preventDefault();
+    // Implement logic to send a POST request to the server with this.state.email
+    try {
+      // Send the request and handle the response
+      const response = await fetch('http://localhost:9000/api/result', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: this.state.email }),
+      });
+
+      if (response.ok) {
+        
+        const data = await response.json();
+        
+        this.setState({ message: data.message });
+        
+        this.setState({ email: '' });
+      } else {
+        // Handle errors
+        // You can update the message state to display an error message
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle network or other errors
+    }
   }
+
 
   render() {
     const { className } = this.props
@@ -72,17 +131,30 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={() => this.move('left')}>LEFT</button>
+          <button id="up" onClick={() => this.move('up')}>UP</button>
+          <button id="right" onClick={() => this.move('right')}>RIGHT</button>
+          <button id="down" onClick={() => this.move('down')}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+        <form onSubmit={this.onSubmit}>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            value={this.state.email}
+            onChange={this.onChange}
+          >
+          </input>
+
+          <input
+            id="submit"
+            type="submit"
+            value='Submit'
+          />
         </form>
       </div>
     )
   }
 }
+
